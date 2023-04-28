20d01308c18aa57bff8def967b88b30b5e5cf4e1
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-04-28 14:32:50 -0400

    Small functions using JavaScript

---
+++ js/test.js
@@ -0,0 +1,14 @@
+function multiply(a, b) {
+return Number(a) + Number(b)
+}
+
+function capitalize(str) {
+    capitalized = str.charAt(0).toUpperCase() + str.slice(1)
+    return capitalized
+}
+
+function lastletter(str) {
+    // returns the very last letter of the string
+}
+
+console.log(capitalize("hohohoh"))
