ed310402f133a1f8ffd6ea3d3fbcd5c4a5f96c61
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-01 10:48:54 -0400

    Add code for rock paper scissors

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 
 function multiply(a, b) {
-return Number(a) + Number(b)
+    return Number(a) + Number(b)
 }
 
 function capitalize(str) {
@@ -11,22 +11,45 @@ function capitalize(str) {
 function lastletter(str) {
     // returns the very last letter of the string
     length = str.length
-    return(str.slice(length-1))
+    return (str.slice(length - 1))
 }
 
 function getComputerChoice() {
-    array = ["rock", "paper","scissors"]
+    array = ["rock", "paper", "scissors"]
     // get random number from 1-3
+
     number = Math.floor(Math.random() * 3)
     return array[number]
 }
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
+    // scissors beats paper
+    // paper beats rock
+    // rock beats scissors
+    winners = [
+        ["scissors", "paper"],
+        ["rock", "scissors"],
+        ["paper", "rock"]
+    ]
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
+    if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    }
 
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 3; i++) {
+           
+        }
+}
+
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Your choice (rock/paper/scissors): ");
+        let computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+}
+
 
